feb30a57e93f11d35aa47e3e759f3533d67ea6d8
Lei Yuan <lei.yuan@colorado.edu>
2023-10-06 14:57:44 -0600

    added requirements for generating stimuli

--- Generating stimuli parameters/R code/stimuli creation.R
+++ Generating stimuli parameters/R code/stimuli creation.R
@@ -1,4 +1,6 @@
 ######################### Generate learning data #######################
+##### select 120 combos from the 720 non-repeating possibilities, for each block, set 20 for learning, 12 for testing.
+
 
 # Load necessary libraries
 library(combinat)
@@ -66,44 +68,8 @@ combined_df <- rbind(df, df_new)
 write.csv(combined_df, "data_source.csv", row.names = FALSE)
 
 ######################### Generate testing data #######################---still working on this. 
-##### Create foil items that are not present during learning to be used as the foil item
-# Randomly select 10 items from the syllabus pool
-selected_items <- sample(syllabus_pool, 10)
-
-# Generate all possible permutations of the selected items
-all_permutations <- expand.grid(selected_items, selected_items, selected_items)
-
-# Filter out valid permutations (those without repeated items)
-valid_permutations <- all_permutations[all_permutations$Var1 != all_permutations$Var2 & 
-                                         all_permutations$Var1 != all_permutations$Var3 & 
-                                         all_permutations$Var2 != all_permutations$Var3, ]
-
-# Identify the invalid permutations (those not in valid_permutations)
-invalid_permutations <- anti_join(all_permutations, valid_permutations)
-
-# Check the size of invalid_permutations
-if (nrow(invalid_permutations) >= 10) {
-  # Randomly select 10 trials from the invalid permutations to be used as foil items
-  foil_items <- sample_n(invalid_permutations, 10)
-} else {
-  # Handle the case where there aren't enough invalid permutations
-  message("Not enough invalid permutations. Consider re-sampling or adjusting criteria.")
-}
-
-##### Select same 10 trials from learning from each conditions as the correct answer
-# Randomly select 10 trial numbers
-selected_trial_numbers <- sample(unique(combined_df$trial), 10)
-
-# Filter the combined_df to include only rows with the selected trial numbers for both conditions
-final_selected_trials_by_number <- combined_df %>%
-  filter(trial %in% selected_trial_numbers)
-
-# Extract 10 unique trial numbers from combined_df
-unique_trial_numbers <- unique(final_selected_trials_by_number$trial)
-
-# Append these trial numbers to foil_items
-foil_items$trial <- unique_trial_numbers 
-
-# Join combined_df with foil_items based on the trial number
-final_df <- left_join(final_selected_trials_by_number, foil_items, by = "trial")
+##### generating foils (visual), they are two types of foil
+#### type 1 (6 trial each block): transposition. for example: ABC, can be transposed into two way: BCA, CAB
+#### type 2 (6 trial each block): one item change, for example: ABC can be paired with DBC (D is randomly chose from 10-3 = 7 possible symbols). 
+#### There are a total of 3 possible changes (left place, middle, right place), creat two trials for each of the change
 
